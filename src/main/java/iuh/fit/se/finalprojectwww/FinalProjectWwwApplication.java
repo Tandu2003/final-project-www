c16855103d22c3d743b5b4143fb5165a7b2c6abb
Tấn Dự <tandu542003@gmail.com>
2025-09-30 16:22:16 +0700

    Initialize Spring Boot project with Maven wrapper and configuration files. Added .gitignore, .gitattributes, and basic application structure including main application class, properties, and test class.

---
+++ src/main/java/iuh/fit/se/finalprojectwww/FinalProjectWwwApplication.java
@@ -0,0 +1,13 @@
+package iuh.fit.se.finalprojectwww;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectWwwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjectWwwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/iuh/fit/se/finalprojectwww/FinalProjectWwwApplicationTests.java
@@ -0,0 +1,13 @@
+package iuh.fit.se.finalprojectwww;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectWwwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
